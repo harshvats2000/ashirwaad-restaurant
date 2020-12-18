d05f86bf8bd799407dda38ecb606755b8b964f35
Harsh Vats <vatsvatsharsh2000@gmail.com>
2020-12-18 21:19:15 +0530

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,59 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = props => {
+  const { data } = props
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Our Menu</h1>
+      {data.allMarkdownRemark.edges.map((menu, i) => (
+        <div key={i}>
+          <Img
+            fixed={menu.node.frontmatter.image.childImageSharp.fixed}
+            alt={menu.node.frontmatter.title}
+          />
+          <br />
+          {menu.node.frontmatter.category}
+          <br />
+          {menu.node.frontmatter.title}
+          <br />₹ {menu.node.frontmatter.price}
+          <br />
+          {menu.node.frontmatter.description}
+          <hr />
+        </div>
+      ))}
+      <Link to="/page-2/">Go to page 2</Link> <br />
+    </Layout>
+  )
+}
+
+export const menuQuery = graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            category
+            title
+            price
+            description
+            image {
+              childImageSharp {
+                fixed(width: 200) {
+                  ...GatsbyImageSharpFixed
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
