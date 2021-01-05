054a79b1411b2d5ffdf4e2b7263f0c033a7ac1c9
Abhishek Ranjan <abhishekranjan0505@gmail.com>
2021-01-05 15:13:23 +0530

    icon of hambergur changed to div -- all 5 errors resolved

--- src/components/header.js
+++ src/components/header.js
@@ -5,17 +5,12 @@ import classes from "./header.module.css"
 const Header = ({ siteTitle }) => (
   <header className={classes.Header}>
     <nav className={classes.navbar}>
-      <label className={classes.navbarToggle} for="chkToggle">
-        <svg
-          fill="white"
-          width="30px"
-          viewBox="0 -53 384 384"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="m368 154.667969h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0" />
-          <path d="m368 32h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0" />
-          <path d="m368 277.332031h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0" />
-        </svg>
+      <label className={classes.navbarToggle} htmlFor="chkToggle">
+        <div>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </label>
       <Link to="/" className={classes.logo}>
         ASHIRWAAD
