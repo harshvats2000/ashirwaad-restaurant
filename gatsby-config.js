6345bd2817fb92c5ecb7288dce9e6eb14307af55
harsh vats <harsh@xamidea.in>
2021-06-23 23:02:27 +0530

    bug: fixed fouc due to styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,18 +18,9 @@ module.exports = {
     `gatsby-transformer-remark`,
     `netlify-cms-app`,
     `gatsby-plugin-netlify-cms`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -44,8 +35,5 @@ module.exports = {
         path: `${__dirname}/content/gallery`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
